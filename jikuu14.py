aa4ed1e220f69d0b06c0500e8182ddded9a5f16b
yontiro123 <jonatantal13@gmail.com>
2025-05-13 21:58:36 +0300

    Update jikuu14.py

--- jikuu14.py
+++ jikuu14.py
@@ -4,6 +4,8 @@ from discord import app_commands
 from datetime import datetime
 import json
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
